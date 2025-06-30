2cf5c01f8423a2b99faef28b25cf99882737f85d
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2025-07-01 02:18:02 +0700

    wip

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,7 @@ import {
 import {
     fetchFile,
     Cleaner,
-    checkResolutions,
-    extractVideoLink,
+    extractVideoLinks,
     extractAudioLink,
     extractTitle,
 } from "../utils";
@@ -24,18 +23,8 @@ export default class Home extends React.Component {
         chunkSize: 0,
         videoSrc: "",
         resourceStr: "",
-        resolutions: {
-            "144p": false,
-            "180p": false,
-            "240p": false,
-            "270p": false,
-            "360p": false,
-            "480p": false,
-            "540p": false,
-            "720p": false,
-            "1080p": false,
-        },
-        selectedMedia: "",
+        resolutions: [], // Changed to store resolution objects
+        selectedQuality: "", // Tracks selected resolution (e.g., "hd_1080p", "sd_360p")
         loading: false,
         isLoaded: false,
         isModalVisible: false,
@@ -47,12 +36,10 @@ export default class Home extends React.Component {
     };
 
     update = (newObj) => {
-        this.setState((prevState) => {
-            return {
-                ...prevState,
-                ...newObj,
-            };
-        });
+        this.setState((prevState) => ({
+            ...prevState,
+            ...newObj,
+        }));
     };
 
     async componentDidMount() {
@@ -68,10 +55,10 @@ export default class Home extends React.Component {
             if (!this.ffmpeg.isLoaded()) {
                 await this.ffmpeg.load().catch((err) => {
                     console.error(err);
-                    this.update({ error: err });
+                    this.update({ error: err.message });
                 });
                 this.ffmpeg.setProgress(({ ratio }) => {
-                    console.log("parsing", parseInt(ratio) * 100 + "%");
+                    console.log("parsing", parseInt(ratio * 100) + "%");
                 });
             }
             this.update({
@@ -131,32 +118,55 @@ export default class Home extends React.Component {
         const cleaner = new Cleaner(e.target.value);
         let resourceStr = cleaner.clean(["u003C", "\\", "amp;"]).value;
 
-        /*
-        navigator.clipboard.writeText(resourceStr).then(function () {
-            console.log('resourceStr copied to clipboard successfully!');
-        }).catch(function (error) {
-            console.error('Unable to copy resourceStr: ', error);
-        }); */
-
         const title = extractTitle(resourceStr);
         this.update({
             resourceStr,
-            fileName: title
+            fileName: title,
+            resolutions: [],
+            selectedQuality: "",
         });
     };
 
     checkHDhandler = () => {
-        const resolutions = checkResolutions(this.state.resourceStr);
-        this.update({
-            resolutions,
-            isModalVisible: true,
-        });
+        try {
+            const links = extractVideoLinks(this.state.resourceStr);
+            const resolutions = [];
+            if (links.hd) {
+                console.log("HD link found:", links.hd);
+                resolutions.push({
+                    videoId: links.hd.videoId,
+                    qualityClass: links.hd.qualityClass,
+                    qualityLabel: links.hd.qualityLabel,
+                    url: links.hd.url,
+                    key: `hd_${links.hd.qualityLabel}`,
+                });
+            }
+            console.log("SD link found:", links.sd);
+            if (links.sd) {
+                resolutions.push({
+                    videoId: links.sd.videoId,
+                    qualityClass: links.sd.qualityClass,
+                    qualityLabel: links.sd.qualityLabel,
+                    url: links.sd.url,
+                    key: `sd_${links.sd.qualityLabel}`,
+                });
+            }
+            this.update({
+                resolutions,
+                isModalVisible: true,
+            });
+        } catch (error) {
+            this.update({
+                error: error.message,
+                isModalVisible: false,
+            });
+        }
     };
 
     extractLinkHandler = async () => {
-        const { resourceStr, selectedMedia } = this.state;
+        const { resourceStr, selectedQuality, resolutions } = this.state;
         const controller = new AbortController();
-        if (resourceStr && selectedMedia) {
+        if (resourceStr && selectedQuality) {
             try {
                 this.update({
                     loading: true,
@@ -164,12 +174,23 @@ export default class Home extends React.Component {
                     error: "",
                     controller,
                 });
-                const video_link = extractVideoLink(resourceStr, selectedMedia);
-                console.log('video_link', video_link);
+
+                // Find the selected resolution
+                const selectedResolution = resolutions.find(
+                    (res) => res.key === selectedQuality
+                );
+                if (!selectedResolution) {
+                    throw new Error("Selected resolution not found.");
+                }
+
+                const video_link = selectedResolution.url;
+                console.log("video_link", video_link);
                 if (!video_link) throw new Error("Video link not found.");
+
                 const audio_link = extractAudioLink(resourceStr);
-                console.log('audio_link', audio_link);
+                console.log("audio_link", audio_link);
                 if (!audio_link) throw new Error("Audio link not found.");
+
                 const getContentLength = (length) =>
                     this.update({
                         contentLength: length + this.state.contentLength,
@@ -193,7 +214,7 @@ export default class Home extends React.Component {
                     progress,
                     handleError,
                     controller,
-                    proxy: this.state.proxy
+                    proxy: this.state.proxy,
                 });
 
                 if (!this.state.error) {
@@ -225,9 +246,10 @@ export default class Home extends React.Component {
 
     selectMedia = (e) => {
         this.update({
-            selectedMedia: e.target.value,
+            selectedQuality: e.target.value,
         });
     };
+
     hideModal = () => {
         this.update({
             isModalVisible: false,
@@ -236,7 +258,7 @@ export default class Home extends React.Component {
 
     cleanVideo = () => {
         const videoSrc = this.state.videoSrc;
-        this.update({ videoSrc: "", resourceStr: "" });
+        this.update({ videoSrc: "", resourceStr: "", resolutions: [], selectedQuality: "" });
         URL.revokeObjectURL(videoSrc);
     };
 
@@ -249,9 +271,7 @@ export default class Home extends React.Component {
     };
 
     render() {
-        const contentLengthInMB = (this.state.contentLength / 1048576).toFixed(
-            2
-        );
+        const contentLengthInMB = (this.state.contentLength / 1048576).toFixed(2);
         const chunkSizeInMB = (this.state.chunkSize / 1048576).toFixed(2);
         return (
             <>
@@ -291,12 +311,17 @@ export default class Home extends React.Component {
                                     <input
                                         id="proxy"
                                         checked={this.state.proxy}
-                                        onChange={
-                                            e => this.setState({ proxy: e.target.checked })}
-                                        type="checkbox" />
-                                    <label style={{
-                                        color: 'black'
-                                    }} htmlFor="proxy">
+                                        onChange={(e) =>
+                                            this.setState({ proxy: e.target.checked })
+                                        }
+                                        type="checkbox"
+                                    />
+                                    <label
+                                        style={{
+                                            color: "black",
+                                        }}
+                                        htmlFor="proxy"
+                                    >
                                         <abbr title="If download has stopped, enable proxy">
                                             Proxy
                                         </abbr>
@@ -365,13 +390,13 @@ export default class Home extends React.Component {
                         </button>
                         <button
                             className={
-                                this.state.loading || !this.state.selectedMedia
+                                this.state.loading || !this.state.selectedQuality
                                     ? "button disabled"
                                     : "button"
                             }
                             onClick={this.extractLinkHandler}
                             disabled={
-                                this.state.loading || !this.state.selectedMedia
+                                this.state.loading || !this.state.selectedQuality
                             }
                         >
                             Download
@@ -478,16 +503,16 @@ export default class Home extends React.Component {
                         padding: 0 8px;
                     }
                     .flex {
-                        display:flex;
+                        display: flex;
                     }
                     .flex-grow {
-                        flex-grow:1;
+                        flex-grow: 1;
                     }
                     .items-center {
-                        align-items:center;
+                        align-items: center;
                     }
                     .gap-3 {
-                        gap:12px;
+                        gap: 12px;
                     }
                     .save-button {
                         min-width: 25px;
--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,5 @@
 export const fetchFile = async (_data, options = {}) => {
+    // Your existing fetchFile function remains unchanged
     const proxyUrl = '/api/proxy?url=' + encodeURIComponent(_data);
     const { getContentLength, progress, handleError, controller, proxy } = options;
     let data;
@@ -55,6 +56,21 @@ export class Cleaner {
         this.value = raw_text;
     }
     clean(trashWords = []) {
+        // Handle common JSON-escaped characters
+        const escapeMap = {
+            "u003C": "<",
+            "u003E": ">",
+            "u002F": "/",
+            "u0026": "&",
+            "u00253D": "=",
+            "u0025": "%",
+            "\\": "",
+            "amp;": "&",
+        };
+        this.value = Object.keys(escapeMap).reduce(
+            (text, key) => text.replaceAll(key, escapeMap[key]),
+            this.value
+        );
         trashWords.forEach((trash) => {
             this.value = this.value.replaceAll(trash, "");
         });
@@ -87,39 +103,6 @@ export function solveCors(link) {
     return link.replace(regex, ".xx");
 }
 
-export function extractLink(str, regex) {
-    const matches = (str + "").match(regex);
-    const extractedResult = matches?.length > 0 ? matches[0] : "";
-    return solveCors(extractedResult);
-}
-
-export function extractVideoLink(str, media) {
-    const regex = new RegExp(
-        '(?<=FBQualityLabel="' +
-        media +
-        '">BaseURL>)(.*?)(?=/BaseURL)',
-        "s"
-    );
-    return extractLink(str, regex);
-}
-
-export function extractAudioLink(str) {
-    const { audioId } = getIds(str);
-    const extractedResult = extractUrl(str, audioId);
-    return solveCors(extractedResult);
-}
-
-export function extractTitle(inputString) {
-    const pattern = /"story":\s*{"message":\s*{"text":"([^"]+)",/;
-    const match = inputString.match(pattern);
-
-    if (match) {
-        return match[1];
-    } else {
-        return "";
-    }
-}
-
 export function getIds(resourceStr) {
     const pattern = /"dash_prefetch_experimental":\[\s*"(\d+v)",\s*"(\d+a)"\s*\]/;
     const match = resourceStr.match(pattern);
@@ -127,10 +110,83 @@ export function getIds(resourceStr) {
         return {
             videoId: match[1],
             audioId: match[2]
-        }
+        };
     } else {
         throw new Error("No match found");
     }
+}
+
+export function extractVideoLinks(str) {
+    const { videoId } = getIds(str);
+    console.log("videoId:", videoId);
+
+    // Clean the entire input string
+    const cleaner = new Cleaner(str);
+    const cleanedStr = cleaner.clean().value;
+
+    // Regex to match Representation blocks
+    const representationRegex = /<Representation\s+[^>]*id="(\d+v)"[^>]*FBQualityClass="([^"]+)"[^>]*FBQualityLabel="([^"]+)"[^>]*>[\s\S]*?<BaseURL>(https:\/\/[^<]+)<\/BaseURL>/g;
+    const representations = [];
+    let match;
+
+    // Collect all representation matches
+    while ((match = representationRegex.exec(cleanedStr)) !== null) {
+        // Include specific IDs or match the videoId from getIds
+        console.log("Match found:", match);
+        console.log("Video ID in match:", match[1]);
+        if (match[1] === videoId) {
+            representations.push({
+                videoId: match[1],
+                qualityClass: match[2],
+                qualityLabel: match[3],
+                url: match[4],
+            });
+        }
+    }
+
+    if (representations.length === 0) {
+        throw new Error("No video representations found for the specified or target video IDs");
+    }
+
+    // Define resolution order for comparison
+    const resolutionOrder = ["144p", "180p", "240p", "270p", "360p", "480p", "540p", "720p", "1080p"];
+
+    // Filter and sort for HD (highest resolution, prefer 1080p)
+    const hdRepresentations = representations
+        .filter((rep) => rep.qualityClass === "hd")
+        .sort((a, b) => resolutionOrder.indexOf(b.qualityLabel) - resolutionOrder.indexOf(a.qualityLabel));
+
+    // Filter and sort for SD (lowest resolution, minimum 360p)
+    const sdRepresentations = representations
+        .filter((rep) => rep.qualityClass === "sd" && resolutionOrder.indexOf(rep.qualityLabel) >= resolutionOrder.indexOf("360p"))
+        .sort((a, b) => resolutionOrder.indexOf(a.qualityLabel) - resolutionOrder.indexOf(b.qualityLabel));
+
+    const result = {
+        hd: hdRepresentations.length > 0 ? {
+            videoId: hdRepresentations[0].videoId,
+            qualityClass: hdRepresentations[0].qualityClass,
+            qualityLabel: hdRepresentations[0].qualityLabel,
+            url: solveCors(hdRepresentations[0].url),
+        } : null,
+        sd: sdRepresentations.length > 0 ? {
+            videoId: sdRepresentations[0].videoId,
+            qualityClass: sdRepresentations[0].qualityClass,
+            qualityLabel: sdRepresentations[0].qualityLabel,
+            url: solveCors(sdRepresentations[0].url),
+        } : null,
+    };
+
+    if (!result.hd && !result.sd) {
+        throw new Error("No valid HD or SD links found for the specified or target video IDs");
+    }
+
+    return result;
+}
+
+export function extractAudioLink(str) {
+    const { audioId } = getIds(str);
+    const extractedResult = extractUrl(str, audioId);
+    return solveCors(extractedResult);
 }
 
 export function extractUrl(fullText, idValue) {
@@ -142,7 +198,6 @@ export function extractUrl(fullText, idValue) {
     while (endIndex !== -1 && endIndex <= startIndex) {
         endIndex = fullText.indexOf("/BaseURL>", endIndex + 1) + end.length;
     }
-    console.log({ startIndex, endIndex })
 
     if (startIndex !== -1 && endIndex !== -1 && startIndex < endIndex) {
         const extractedText = fullText.substring(startIndex, endIndex);
@@ -156,5 +211,16 @@ export function extractUrl(fullText, idValue) {
         }
     } else {
         throw new Error("Invalid range");
+    }
+}
+
+export function extractTitle(inputString) {
+    const pattern = /"story":\s*{"message":\s*{"text":"([^"]+)",/;
+    const match = inputString.match(pattern);
+
+    if (match) {
+        return match[1];
+    } else {
+        return "";
     }
 }
